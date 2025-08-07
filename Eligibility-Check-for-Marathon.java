5e21f740a6784342baf76dbaf55922ee83285ea2
priya-Gajenthiran <priyagajendran05@gmail.com>
2025-08-07 12:45:16 +0530

    Create Eligibility-Check-for-Marathon.java

---
+++ Eligibility-Check-for-Marathon.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc= new Scanner(System.in);
+        int age= sc.nextInt();
+        int fit = sc.nextInt();
+        if(age>=18 && fit>=60){
+            System.out.println("Eligible for marathon");
+        }
+        else{
+            System.out.println("Not eligible for marathon");
+        }
+    }
+}
